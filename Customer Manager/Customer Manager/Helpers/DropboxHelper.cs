65e3fc53b3367be26e053ad9c30cb691c863611f
Jojo Barria <menumbra@gmail.com>
2025-05-28 20:32:07 +0400

    Implemented Dropbox folder upload after customer creation in CustomerViewModel using GetCustomerFolder and UploadFolderAsync

---
+++ Customer Manager/Customer Manager/Helpers/DropboxHelper.cs
@@ -0,0 +1,38 @@
+﻿using Dropbox.Api;
+using Dropbox.Api.Files;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Customer_Manager.Helpers;
+
+public static class DropboxHelper
+{
+    private const string AccessToken = "YOUR_DROPBOX_ACCESS_TOKEN_HERE";
+
+    public static async Task UploadFolderAsync(string localFolderPath, string dropboxFolderPath)
+    {
+        using var dbx = new DropboxClient(AccessToken);
+
+        if (!Directory.Exists(localFolderPath))
+        {
+            throw new DirectoryNotFoundException($"Local folder not found: {localFolderPath}");
+        }
+
+        var files = Directory.GetFiles(localFolderPath, "*", SearchOption.AllDirectories);
+
+        foreach (var file in files)
+        {
+            var relativePath = Path.GetRelativePath(localFolderPath, file).Replace("\\", "/");
+            var dropboxPath = $"{dropboxFolderPath}/{relativePath}";
+
+            using var fileStream = File.OpenRead(file);
+            await dbx.Files.UploadAsync(
+                path: dropboxPath,
+                mode: WriteMode.Overwrite.Instance,
+                autorename: false,
+                body: fileStream);
+
+        }
+    }
+}
---
+++ Customer Manager/Customer Manager/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Customer_Manager.Models;
+using Customer_Manager.Helpers;
+using Customer_Manager.Data;
+
+namespace Customer_Manager.ViewModels;
+
+public class CustomerViewModel
+{
+    private readonly CustomerRepository _customerRepository;
+
+    public ObservableCollection<Customer> Customers { get; set; } = new();
+
+    public CustomerViewModel(string dbPath)
+    {
+        _customerRepository = new CustomerRepository(dbPath);
+    }
+
+    public void AddCustomer(Customer newCustomer)
+    {
+        // Save to database
+        _customerRepository.AddCustomer(newCustomer);
+
+        // Create folder
+        var folderPath = PathHelper.GetCustomerFolder(newCustomer.Editor, newCustomer.Name);
+
+        // Upload to Dropbox
+        var dropboxPath = $"/{newCustomer.Month}/{newCustomer.Date}/{newCustomer.Editor}/{newCustomer.Name}";
+        try
+        {
+            DropboxHelper.UploadFolderAsync(folderPath, dropboxPath).Wait();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Dropbox upload failed: {ex.Message}");
+        }
+
+        // Add to local collection
+        Customers.Add(newCustomer);
+    }
+}
